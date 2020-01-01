8ff5b9cf4070eb8142a7ba7c9ec181d1840317fe
Nibbles <siliconrob@siliconheaven.net>
2019-12-31 23:25:32 -0600

    Read and filter out links from ercot sced html

---
+++ test/readhtml.js
@@ -0,0 +1,34 @@
+const jssoup = require('jssoup').default;
+const request = require('request');
+const querystring = require('querystring');
+
+const reportUrl = new URL('http://mis.ercot.com/misapp/GetReports.do?reportTypeId=13052');
+
+function reject(obj, keys) {
+  return Object.keys(obj)
+      .filter(z => !keys.includes(z))
+      .map(z => Object.assign({}, {[z]: obj[z]}))
+      .reduce((res, o) => Object.assign(res, o), {});
+};
+
+function extractLink(link) {
+  const fullUrl = new URL(`${reportUrl.protocol}//${reportUrl.host}${link.attrs.href}`);
+  if (fullUrl.searchParams.has("mimic_duns") && Number(fullUrl.searchParams.get("mimic_duns")) == 0) {
+    fullUrl.searchParams.delete("mimic_duns");
+  }
+  const filteredParams = fullUrl.searchParams.toString();  
+  const parsedUrl = new URL(`${fullUrl.protocol}//${fullUrl.host}${fullUrl.pathname}?${filteredParams}`);
+  return parsedUrl;
+};
+
+request(reportUrl.href, (error, response, body) => {
+  console.error('error:', error);
+  console.log('statusCode:', response && response.statusCode);
+  //console.log('body:', body);
+  let soup = new jssoup(body);
+  let links = soup.findAll('a');
+  links.map(z => {
+    const link = extractLink(z);
+    console.log(link.href);
+  });
+});
