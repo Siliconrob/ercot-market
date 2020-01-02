d5c42f7abb1f234d9ddc3ebfea4077799495fa47
Nibbles <siliconrob@siliconheaven.net>
2020-01-02 00:17:40 -0600

    Download zip contents and save to temp file

--- test/testreader.js
+++ test/testreader.js
@@ -1,9 +1,29 @@
 const reader = require('./readhtml');
 
-console.log(reader.urlOptions);
+//console.log(reader.urlOptions);
 
-reader.extractSCEDUrls().then(links => {
-  links.forEach(element => {
-    console.log(element);
+const axios = require('axios');
+const streamZip = require('node-stream-zip');
+const fs = require('fs').promises;
+const tempWrite = require('temp-write');
+
+async function zippedData(link) {
+  const response = await axios.get(link);
+  console.log(response);
+  return response.data;
+}
+
+(async function() {
+  const results = await reader.extractSCEDUrls();
+
+  const dl = results.shift();
+  console.log(dl);
+  const response = await axios({
+    url: dl,
+    method: 'GET',
+    responseType: 'arraybuffer',
   });
-});
+
+  const filePath = await tempWrite(Buffer.from(response.data));
+  await fs.writeFile('abc.zip', await fs.readFile(filePath));
+})();
